013893ca9e72283f9d2f6a1f776f00cbfdd61299
yaofa <yaofa@xiaomi.com>
2025-04-01 12:23:44 +0800

    Fix: fix high
    Signed-off-by: yaofa <yaofa@xiaomi.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,7 +10,7 @@ return {
   config = function()
     require("nvim-treesitter.configs").setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-      ensure_installed = { "c", "cpp", "bash", "lua", "vim", "vimdoc", "markdown" },
+      ensure_installed = { "c", "cpp", "bash", "lua", "vim", "vimdoc", "markdown", "go",},
     
       -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
@@ -35,7 +35,7 @@ return {
         -- disable = { "c", "rust" },
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
         disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
+            local max_filesize = 400 * 1024 -- 100 KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
                 return true
